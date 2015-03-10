14647466ef8304f22653855eda8e250601bf1191
Alan deLevie <adelevie@gmail.com>
2015-03-10 18:05:00 -0400

    Fixed tests for ::Page

--- spec/ra11y_page_spec.rb
+++ spec/ra11y_page_spec.rb
@@ -6,18 +6,11 @@ describe "Ra11y::Page" do
     @page = Ra11y::Page.new("home_page", "<html><body></body><img src='foo' alt='redundant'><h1>redundant</h1></img></html>")
   end
 
-  it "stores the path" do
-    expect(@site.path).to eql(fixtures_path)
-  end
-
-  it "knows the path to html files" do
-    expect(@site.paths.count).to eql(3)
-    expected = File.expand_path "about.html", fixtures_path
-    expect(@site.paths.first).to eql(expected)
-  end
-
-  it "builds the HTML files" do
-    expect(@site.html_files.count).to eql(3)
-    expect(@site.html_files.first.class).to eql(Ra11y::HtmlFile)
+  it "returns errors, warnings, and notices" do
+    errors, warnings, notices = @page.run
+    
+    expect(errors).to(respond_to(:length))
+    expect(warnings).to(respond_to(:length))
+    expect(notices).to(respond_to(:length))
   end
 end
