bea56ecc84a6567687cade9c53e8ae816e7610e2
Ben Balter <ben.balter@github.com>
2015-08-12 15:15:12 -0400

    better output formatting

--- spec/ra11y_result_spec.rb
+++ spec/ra11y_result_spec.rb
@@ -3,20 +3,39 @@ require "spec_helper"
 describe "Ra11y::Result" do
 
   before do
-    @result = Ra11y::Result.new({ "code" => "foo", "message" => "bar", "type" => "error" })
+    @result = Ra11y::Result.new({
+      "code"      => "foo",
+      "message"   => "bar",
+      "type"      => "error",
+      "context"   => "<title>Pa11y - Your automated accessib...</title>",
+      "selector"  => "html > head > title",
+      "typeCode"  => 3,
+    })
   end
 
   it "stores the hash" do
     expect(@result.code).to eql("foo")
     expect(@result.message).to eql("bar")
     expect(@result.type).to eql("error")
+    expect(@result.context).to eql("<title>Pa11y - Your automated accessib...</title>")
+    expect(@result.selector).to eql("html > head > title")
+    expect(@result.type_code).to eql(3)
+  end
+
+  it "returns the hash" do
+    hash = @result.to_hash
+    expect(hash[:code]).to eql("foo")
+    expect(hash[:message]).to eql("bar")
+    expect(hash[:type]).to eql("error")
+    expect(hash[:context]).to eql("<title>Pa11y - Your automated accessib...</title>")
+    expect(hash[:selector]).to eql("html > head > title")
+    expect(hash[:type_code]).to eql(3)
   end
 
   it "knows when its an error" do
     expect(@result.error?).to eql(true)
     expect(@result.warning?).to eql(false)
     expect(@result.notice?).to eql(false)
-
   end
 
   it "knows when its a notice" do
@@ -31,5 +50,9 @@ describe "Ra11y::Result" do
     expect(result.error?).to eql(false)
     expect(result.warning?).to eql(true)
     expect(result.notice?).to eql(false)
+  end
+
+  it "formats the string" do
+    expect(@result.to_s).to match(/message: bar\n/)
   end
 end
